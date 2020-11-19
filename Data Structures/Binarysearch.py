84eaa5ab7b1d348e260ca33bf1b860e9dcb9a622
Prasanna Kumar.M <55314553+Prasanna2002@users.noreply.github.com>
2020-11-19 14:14:14 +0530

    Add files via upload

---
+++ Data Structures/Binarysearch.py
@@ -0,0 +1,17 @@
+x=[i for i in range(1,101)]
+def binarysearch(x):
+    search=int(input("Enter A Number: "))
+    low=0 
+    mid=0
+    high=len(x)-1 
+    while mid<=high and mid<len(x)-1: 
+        mid=(low+high)//2
+        if x[mid]>search: 
+            high=high-1 
+        elif x[mid]<search: 
+            low=mid+1 
+        elif x[mid]==search: 
+            return x[mid]
+    return -1 
+
+print(binarysearch(x))
