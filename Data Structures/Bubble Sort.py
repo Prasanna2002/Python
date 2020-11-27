db2d45e45b8b1e10f936e16abb616e21b4551d20
Prasanna Kumar.M <55314553+Prasanna2002@users.noreply.github.com>
2020-11-27 23:33:49 +0530

    Update Bubble Sort.py

--- Data Structures/Bubble Sort.py
+++ Data Structures/Bubble Sort.py
@@ -1,14 +1,9 @@
-x=[i for i in range(100,0,-1)] 
-d=False 
-while d!=True:
-    r=0
-    for i in range(len(x)-1): 
-        if x[i]>x[i+1]: 
-            x[i],x[i+1]=x[i+1],x[i]
-            r+=1 
-    if r==0: 
-        print(x) 
-        d=True 
+def bubblesort(x): 
+    y=len(x)
+    for j in range(y):
+        for i in range(0,y-j-1): 
+            if x[i]>x[i+1]: 
+                x[i],x[i+1]=x[i+1],x[i]
     
     
 
